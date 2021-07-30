5ef5e6f8cc345a5c20c3e688d38545c557a5a45a
PEDDISHIVANI12 <pshivramiit@gmail.com>
2021-07-30 16:10:44 +0530

    Add Project which was done using Django,basically it was an application to report police about crimes and emergencies in rael time

--- evrysec/accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
- 
-class EmailForm(forms.Form):
-    Place_Of_Incident = forms.CharField(max_length=100)
-    attach = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required = False)
-    Description = forms.CharField(widget = forms.Textarea)
-
--- evrysec/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class reports(models.Model):
-    place = models.CharField(max_length=200)
-    description = models.TextField(max_length=500)
-
-def __str__(self):
-    return self.place
